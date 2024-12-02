b28b82ecbfb40b111f7e37c79a7f99a06df39ca7
Hannes Koivusipilä <hannesi@iki.fi>
2024-12-02 10:44:48 +0200

    work on p2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,4 +1,4 @@
-use std::{cmp::Ordering, collections::HashSet};
+use std::{cmp::Ordering, collections::HashSet, fmt::write};
 
 advent_of_code::solution!(2);
 
@@ -28,12 +28,6 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 fn adjacent_levels_do_not_bounce_excessively(report: &[u32]) -> bool {
-    adjacent_levels_safe_bounce_indicators(report)
-        .iter()
-        .all(|l| *l)
-}
-
-fn adjacent_levels_safe_bounce_indicators(report: &[u32]) -> Vec<bool> {
     report
         .iter()
         .zip(report.iter().skip(1))
@@ -41,7 +35,7 @@ fn adjacent_levels_safe_bounce_indicators(report: &[u32]) -> Vec<bool> {
             let diff = left.abs_diff(*right);
             diff >= 1 && diff <= 3
         })
-        .collect()
+        .all(|l| l)
 }
 
 fn levels_are_ascending_or_descending(report: &[u32]) -> bool {
@@ -51,7 +45,11 @@ fn levels_are_ascending_or_descending(report: &[u32]) -> bool {
         return false;
     }
 
-    let interlevel_bounce_directions = interlevel_bounce_direction_array(report);
+    let interlevel_bounce_directions: Vec<Ordering> = report
+        .iter()
+        .zip(report.iter().skip(1))
+        .map(|(left, right)| left.cmp(right))
+        .collect();
 
     interlevel_bounce_directions
         .iter()
@@ -61,18 +59,42 @@ fn levels_are_ascending_or_descending(report: &[u32]) -> bool {
             .all(|ilbd| *ilbd == Ordering::Greater)
 }
 
-fn interlevel_bounce_direction_array(report: &[u32]) -> Vec<Ordering> {
-    report
-        .iter()
-        .zip(report.iter().skip(1))
-        .map(|(left, right)| left.cmp(right))
-        .collect()
-}
-
 pub fn part_two(input: &str) -> Option<u32> {
     let reports = parse_reports(input);
 
+    let interlevel_relationship_vectors: Vec<Vec<InterlevelRelationship>> = reports
+        .iter()
+        .map(|report| generate_interlevel_relationships(report)).collect();
+
+
     None
+}
+
+fn generate_interlevel_relationships(report: &[u32]) -> Vec<InterlevelRelationship> {
+    report.iter().zip(report.iter().skip(1)).map(|(left, right)| {
+        let direction = left.cmp(right);
+        let bounce_height = left.abs_diff(*right);
+        let is_safe_bounce_distance = bounce_height >= 1 && bounce_height <= 3;
+        InterlevelRelationship::new(direction, is_safe_bounce_distance)
+    }).collect()
+}
+
+struct InterlevelRelationship {
+    direction: Ordering,
+    is_safe_bounce_distance: bool,
+}
+
+impl InterlevelRelationship {
+    pub fn new(direction: Ordering, excessive_bounce: bool) -> Self {
+        Self {
+            direction,
+            is_safe_bounce_distance: excessive_bounce,
+        }
+    }
+
+    pub fn equals(&self, other: &Self) -> bool {
+        self.direction == other.direction && self.is_safe_bounce_distance == other.is_safe_bounce_distance
+    }
 }
 
 #[cfg(test)]
